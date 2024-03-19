5338968e91bb770e74694412999e482af9143732
Jj <JJB9922@protonmail.com>
2024-03-19 21:37:44 +0000

    marker to halfway point

--- dailytenkai/src/app/components/Map.tsx
+++ dailytenkai/src/app/components/Map.tsx
@@ -5,7 +5,7 @@ import { GoogleMap, LoadScript, Autocomplete, Library, Marker } from '@react-goo
 import calculateNewCoordinates from './CoordinateCalc';
 
 const libraries: Library[] = ['places'];
-const halfDistance: number = 4;
+const halfDistance: number = 3.5;
 
 function getRandomInt(min: number, max: number) {
   const minCeiled = Math.ceil(min);
@@ -14,11 +14,12 @@ function getRandomInt(min: number, max: number) {
 }
 
 const MapComponent: React.FC = () => {
-  const mapRef = useRef<GoogleMap | null>(null); 
+  const mapRef = useRef<GoogleMap | null>(null);
   const [isError, setIsError] = useState<boolean>(false);
   const [coordinates, setCoordinates] = useState<{ lat: number; lng: number } | null>(null);
   const [zoom, setZoom] = useState<number>(8);
   const [markerPosition, setMarkerPosition] = useState<{ lat: number; lng: number } | null>(null);
+  const [markerPosition2, setMarkerPosition2] = useState<{ lat: number; lng: number } | null>(null);
   const autocompleteRef = useRef<google.maps.places.Autocomplete>();
 
   const handlePlaceSelect = () => {
@@ -29,14 +30,14 @@ const MapComponent: React.FC = () => {
       const lng = place.geometry.location.lng();
       setCoordinates({ lat, lng });
       setMarkerPosition({ lat, lng });
-      setZoom(15);
+      setZoom(12);
     } else {
       setZoom(8);
     }
   };
 
   const calculateRouteCoordinates = () => {
-    if(coordinates == null){
+    if (coordinates == null) {
 
       setIsError(true);
       setTimeout(() => {
@@ -48,17 +49,59 @@ const MapComponent: React.FC = () => {
       //8km in 10k steps - 4km in 5k
       //distance between 2 points = acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371
       let halfwayPoint = calculateNewCoordinates(coordinates.lat, coordinates.lng,
-                                                 halfDistance, getRandomInt(0, 360));
+        halfDistance, getRandomInt(0, 360));
 
       console.log(halfwayPoint.dLat2, ", ", halfwayPoint.dLon2);
+      let lat = halfwayPoint.dLat2;
+      let lng = halfwayPoint.dLon2;
+
+      checkIfInWater(lat, lng)
+        .then((isInWater) => {
+          if (isInWater) {
+            calculateRouteCoordinates();
+          } else {
+            setMarkerPosition2({ lat, lng });
+          }
+        })
+        .catch((error) => {
+          console.error('Error checking if coordinates are in water:', error);
+        });
     }
   };
+
+  const checkIfInWater = (lat: number, lng: number) => {
+    return new Promise((resolve, reject) => {
+
+      const geocoder = new google.maps.Geocoder();
+      const latlng = new google.maps.LatLng(lat, lng);
+
+      geocoder.geocode({ 'location': latlng }, (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results == null) return;
+          if (results[1]) {
+            const addressComponents = results[1].address_components;
+
+            const isInWater = addressComponents.some(component => component.short_name.length < 2) 
+            || addressComponents.some(component => component.types.includes('water'))
+            || addressComponents.length < 2;
+            console.log("Addr", addressComponents);
+            resolve(isInWater);
+          } else {
+            resolve(false); 
+          }
+        } else {
+          reject(status);
+        }
+      });
+    });
+  };
+
 
   return (
     <div>
       <LoadScript
         googleMapsApiKey={process.env.
-          NEXT_PUBLIC_GOOGLE_MAPS_API_KEY|| ''}
+          NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
         libraries={libraries}
       >
         <Autocomplete onLoad={autocomplete => (autocompleteRef.current = autocomplete)} onPlaceChanged={handlePlaceSelect}>
@@ -70,12 +113,13 @@ const MapComponent: React.FC = () => {
           center={coordinates || { lat: -34.397, lng: 150.644 }}
           zoom={zoom}
         >
-        {markerPosition && <Marker position={markerPosition} />}
+          {markerPosition && <Marker position={markerPosition} />}
+          {markerPosition2 && <Marker position={markerPosition2} />}
         </GoogleMap>
       </LoadScript>
       <div className='flex md:flex-row flex-col items-center justify-between md:py-4 gap-4 py-4 px-4'>
         <p>Selected Coordinates: {coordinates ? `${coordinates.lat}, ${coordinates.lng}` : "None"}</p>
-        <button type="button" className="dark:text-kachi text-shironeri bg-kachi dark:bg-shironeri hover:shadow-[0_5px_12px_rgb(0,0,0,0.2)] rounded-lg px-5 py-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.2)]" onClick={calculateRouteCoordinates}>Generate Route</button>      
+        <button type="button" className="dark:text-kachi text-shironeri bg-kachi dark:bg-shironeri hover:shadow-[0_5px_12px_rgb(0,0,0,0.2)] rounded-lg px-5 py-2.5 shadow-[0_3px_10px_rgb(0,0,0,0.2)]" onClick={calculateRouteCoordinates}>Generate Route</button>
       </div>
 
       {isError && (
@@ -83,7 +127,7 @@ const MapComponent: React.FC = () => {
           <span className="text-lg text-ichigo dark:text-usubeni">Error! Please select a location.</span>
         </div>
       )}
-      
+
     </div>
   );
 };
